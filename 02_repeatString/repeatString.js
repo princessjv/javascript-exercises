db793953dc49bf0ebfd274f5aadd8a32522b072a
Princess <53481093+princessjv@users.noreply.github.com>
2023-07-26 14:05:07 +0800

    >

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,10 @@
-const repeatString = function(string='hey',num) {
-    let strings=""; //initily
-
-    for(let count=0;
-        count<num;
-        ++count
-        ){
-            strings+=string;//accumulate the string
-        }
-        return strings;
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  
+  module.exports = repeatString;
